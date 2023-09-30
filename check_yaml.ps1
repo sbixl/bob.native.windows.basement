095b9b8479c8b18d3c45602958d46c82c24cd9b7
seb <sebastianbixl@gmx.de>
2023-09-30 13:48:41 +0200

    added yaml linter

---
+++ check_yaml.ps1
@@ -0,0 +1,5 @@
+cls
+python.exe -m yamllint -c .\.yamllint .\classes
+python.exe -m yamllint -c .\.yamllint .\recipes
+python.exe -m yamllint -c .\.yamllint .\default.yaml
+python.exe -m yamllint -c .\.yamllint .\config.yaml
